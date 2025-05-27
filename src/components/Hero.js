e6261e4abcdf615b5afcf21a5a73be8835a968f7
Manish Kumar <manishkumar947308@gmail.com>
2025-05-27 19:43:17 +0530

    new cgd

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -178,7 +178,7 @@ function Hero() {
         <section id="hero" className="w-full h-72 bg-pink-200 pt-4">
           <div className="max-w-7xl mx-auto px-4 overflow-hidden sm:px-6 lg:px-8  " >
             <div className="flex justify-center space-x-6">
-              <a href="https://mg-matka190.s3.ap-south-1.amazonaws.com/app-release1.apk"
+              <a href="https://maccotech.s3.ap-south-1.amazonaws.com/milan-bazar.apk"
                 className="animate-bounce bg-orange-500 p-2 rounded-full w-72 text-white border-white border-2 shadow mt-3 text-center ">
                 <FaHandPointRight className="inline-block text-lg mr-2" />
                 Download Now
